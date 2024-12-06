b2137f9c6f5499c721c404d25f6799d6e604111c
mjpurin <mjpurin@gmail.com>
2024-12-06 13:15:49 +0900

    チェックボックス形式に変更

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const DEFAULT_IMPORTANCE = "3";
     const todoInput = document.querySelector('#todoInput'); 
     const importance = document.querySelector('#importance'); 
-    const addBtn = document.querySelector('#addBtn'); 
     const todoList = document.querySelector('#todoList'); 
     let list=[];
 
@@ -19,17 +17,18 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem('todoList', JSON.stringify(list));
     };
 
-    addBtn.addEventListener('click', () => {
+    importance.addEventListener('change', () => {
         const todo = todoInput.value.trim();
         if (!todo) {
             alert('Todoを入力してください！');
+            importance.value="0";
             return;
         }
         const importanceValue = parseInt(importance.value);
         const item = { todo, importanceValue };
         list.push(item);
         todoInput.value = '';
-        importance.value = DEFAULT_IMPORTANCE;
+        importance.value = 0;
         sortList();
         displayList();
         saveList(); 
@@ -39,43 +38,48 @@ document.addEventListener('DOMContentLoaded', () => {
         list.sort((a, b) => b.importanceValue - a.importanceValue);
     };
 
-    todoList.addEventListener('click', (eve) => {
-        const idx = eve.target.closest('li').dataset.index;
-        if (eve.target.textContent === '完了') {
-            list[idx].importanceValue = 0;
-            sortList();
+
+    const checkedListener = (eve) => {
+        const li = eve.target.closest('li')
+        const idx = li.dataset.index;
+        list.splice(idx, 1);
+        li.animate(
+            {
+                opacity:[1,0],
+                translate:[0,"10px"],
+            },
+            {
+                duration:600,
+                easeing:"ease",
+                fill:"forwards"
+            }
+        ).finished.then(()=>{
+            li.style.display = 'none';
             displayList();
             saveList(); 
-        } else if (eve.target.textContent === '削除') {
-            list.splice(idx, 1);
-            displayList();
-            saveList(); 
-        }
-    });
+        });
+    };
 
     const displayList = () => {
         todoList.innerHTML = '';
         list.forEach((item, index) => {
             const li = document.createElement('li');
             li.dataset.index = index;
+            const cb = document.createElement("input");
+            cb.type="checkbox";
+            cb.addEventListener("change",checkedListener);
+            li.appendChild(cb);
             const span = document.createElement('span');
             span.textContent = item.todo;
             if (item.importanceValue === 0) {
                 span.classList.add('finished');
             }
             li.appendChild(span);
-            
+
             // 重要度に応じた星の表示
             const importanceSpan = document.createElement('span');
             importanceSpan.textContent = '★'.repeat(item.importanceValue);;
             li.appendChild(importanceSpan);
-
-            const bt1 = document.createElement('button');
-            bt1.textContent = '完了';
-            li.appendChild(bt1);
-            const bt2 = document.createElement('button');
-            bt2.textContent = '削除';
-            li.appendChild(bt2);
 
             todoList.appendChild(li);
         });
